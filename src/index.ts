c7fda97a1a126616e71034cf9b6ae522d5ae01d0
Azat Azamat <azatkariuly@Azats-MacBook-Pro.local>
2024-10-19 17:13:41 +0900

    fix: db port order

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,6 @@ import 'dotenv/config';
 import authRoute from './routes/auth.route.js';
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_CONNECT)
-  .then(() => (
-    console.log('DB connected')
-  ));
-
 const port = 3000
 
 const app = express()
@@ -21,6 +16,10 @@ app.get('/', (req, res) => {
 const URL = '/api';
 app.use(URL + '/user', authRoute);
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
-})
+
+mongoose.connect(process.env.MONGODB_CONNECT)
+  .then(() => (
+    app.listen(port, () => {
+        console.log(`App listening at http://localhost:${port}`)
+    })
+  ));
