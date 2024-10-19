ed323ed9354d5f6a8ef7bdd9484f99d71ab60b1e
Azat Azamat <azatkariuly@Azats-MacBook-Pro.local>
2024-10-19 17:25:14 +0900

    update: vercel rule

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express()
 
 const corsConfig = {
     origin: '*',
-    credentials: true,
+    credential: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
 app.options('', cors(corsConfig))
